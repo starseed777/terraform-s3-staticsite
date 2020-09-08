6b1d7043a72c8e70d5655f4df967422677974895
starseed777 <waress66@live.com>
2020-09-08 14:33:44 -0400

    static website hosting added to s3 bucket

--- infra.tf
+++ infra.tf
@@ -1,4 +1,34 @@
 provider "aws" {
   version = "~> 3.0"
+  profile = "default"
   region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "blog-bucket" {
+  bucket = "blogjawnt"
+  acl    = "public-read"
+  policy = file("policy.json")
+
+  tags = {
+    Name        = "blog bucket"
+    Environment = "Dev"
+  }
+
+
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
 }
