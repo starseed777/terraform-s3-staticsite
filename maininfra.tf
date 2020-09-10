f9318cd38fda07da19df1e3b1a150abf92da98f9
starseed777 <waress66@live.com>
2020-09-10 01:11:57 -0400

    changed infra file name + backend setup to existing s3 bucket

---
+++ maininfra.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  version = "~> 3.0"
+  profile = var.aws-profile
+  region  = var.aws-region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "backupjawnts"
+    key    = "terraform/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+module "blog" {
+  source = "./s3mod"
+}
+
+module "dev-server" {
+  source = "./ec2mod"
+}
+
+
