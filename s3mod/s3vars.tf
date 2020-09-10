9503b3065dd0362608b823184417bf531974e9dc
starseed777 <waress66@live.com>
2020-09-10 01:07:23 -0400

    some variables for s3 code

---
+++ s3mod/s3vars.tf
@@ -0,0 +1,15 @@
+variable bucketname {
+  type        = string
+  default     = "blogjawnt"
+}
+
+variable tagname {
+    type = string
+    default = "blog-bucket"
+}
+
+variable bucketenv {
+  type        = string
+  default     = "dev"
+}
+
